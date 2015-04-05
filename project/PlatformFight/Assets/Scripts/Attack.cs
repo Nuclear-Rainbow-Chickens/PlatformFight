c9aa87793999e5995fa09a8ccec6af695098995e
supermiu <jeffersonlee2000@gmail.com>
2015-04-04 19:39:38 -0700

    basic sprites

---
+++ project/PlatformFight/Assets/Scripts/Attack.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+
+	GameObject meleeObj;
+	GameObject rangedObj;
+	// Use this for initialization
+	void Start () {
+		meleeObj = transform.parent.GetChild (2).gameObject;
+		rangedObj = transform.parent.GetChild (1).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Z)) {
+			meleeObj.SetActive(true);
+			gameObject.SetActive(false);
+		}
+		if(Input.GetKeyDown(KeyCode.X)) {
+			rangedObj.SetActive(true);
+			gameObject.SetActive(false);
+		}
+	}
+}
---
+++ project/PlatformFight/Assets/Scripts/ResetAttack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetAttack : MonoBehaviour {
+	Animator anim;
+	GameObject idlearm;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		idlearm = transform.parent.GetChild (0).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (anim.GetCurrentAnimatorStateInfo (0).IsName ("idle")) {
+			idlearm.SetActive(true);
+			gameObject.SetActive(false);
+			Debug.Log ("melee attack");
+		}
+	}
+}
