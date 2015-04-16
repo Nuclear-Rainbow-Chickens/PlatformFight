2bd233ee91fc2467372a0699b073a8b66505d026
supermiu <jeffersonlee2000@gmail.com>
2015-04-15 19:50:00 -0700

    uglier

--- project/PlatformFight/Assets/Scripts/Sync.cs
+++ project/PlatformFight/Assets/Scripts/Sync.cs
@@ -14,29 +14,24 @@ public class Sync : MonoBehaviour {
 		Vector3 position = Vector3.zero;
 		Vector3 velocity = Vector3.zero;
 		float rotation = 0;
-		Vector3 xScale = Vector3.zero;
 
 		if (stream.isWriting)
 		{
 			position = rb.position;
 			velocity = rb.velocity;
 			rotation = rb.rotation;
-			xScale = transform.localScale;
 			stream.Serialize(ref position);
 			stream.Serialize(ref velocity);
 			stream.Serialize(ref rotation);
-			stream.Serialize(ref xScale);
 		}
 		else
 		{
 			stream.Serialize(ref position);
 			stream.Serialize(ref velocity);
 			stream.Serialize(ref rotation);
-			stream.Serialize(ref xScale);
 			rb.position = position;
 			rb.velocity = velocity;
 			rb.rotation = rotation;
-			transform.localScale = xScale;
 		}
 		
 	}
